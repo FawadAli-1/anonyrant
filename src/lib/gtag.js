a8b0ebf464a1f7988eb90d22ac28bdbe31ac928e
Fawad Ali <becomebetter101@gmail.com>
2025-05-23 12:16:14 +0500

    Added google and vercel analytics

---
+++ src/lib/gtag.js
@@ -0,0 +1,8 @@
+// lib/gtag.js or gtag.ts
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+export const pageview = (url) => {
+  window.gtag('config', GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
